4fdb7b8a29bfb5d303f31e2e277080390c074904
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-12-10 15:44:34 -0600

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>Wazzup?</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
